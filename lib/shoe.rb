78089f2839832c96fe7be167bbdfbbed0f711950
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-17 20:49:28 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+ class Shoe
+   attr_accessor :color, :size, :material, :condition
+     def cobble
+      puts "Your shoe is as good as new!"
+      @condition = "new"
+    end
+    attr_reader :brand # these are methods puts be on this line
+      
+    def initialize(brand)
+     @brand = brand
+    end
+  
+end
+ 
