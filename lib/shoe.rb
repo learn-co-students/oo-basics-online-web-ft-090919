78b15fd239d325172fcdd9171a6780063fffd19a
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-24 02:11:37 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,16 @@
- class Shoe
-   attr_accessor :color, :size, :material, :condition
-     def cobble
-      puts "Your shoe is as good as new!"
+class Shoe
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand = brand
+    @condition = "old"
+  end
+  
+  def cobble
+    puts "Your shoe is as good as new!"
       @condition = "new"
-    end
-    attr_reader :brand # these are methods puts be on this line
-      
-    def initialize(brand)
-     @brand = brand
-    end
+  end
+  
   
 end
- 
